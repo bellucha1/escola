22cff6f1fb2364d9ae07ab70383bdb51bca83a2d
bellucha1 <isabella.wichnoski@escola.pr.gov.br>
2026-03-18 07:36:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão
+const btn = document.getElementById('mensagemBtn');
+
+// Adiciona evento de clique
+btn.addEventListener('click', () => {
+    alert('Seja bem-vindo(a) ao Colégio Estadual São Cristóvão!');
+});
